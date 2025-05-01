9443bdf9f2d0d06c71d462cc5ecaf7f3cc9bca75
GuilhermeRamos1240702 <guipintoramos@gmail.com>
2025-05-01 23:51:10 +0100

    init commit

--- src/Barraca.java
+++ src/Barraca.java
@@ -1,3 +1,18 @@
-public class Barraca {
+public class Barraca extends Federaçao {
 
+    int voluntario;
+
+    private static final int numero_min_funcionarios = 2;
+
+    public Barraca(int voluntario){
+        this.voluntario=voluntario;
+    }
+
+    public int getVoluntario() {
+        return voluntario;
+    }
+
+    public void setVoluntario(int voluntario) {
+        this.voluntario = voluntario;
+    }
 }
