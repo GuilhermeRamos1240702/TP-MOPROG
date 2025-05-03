5ce6d33bb55b7dc6e8588d24eb5d72eaeaa22d0f
GuilhermeRamos1240702 <guipintoramos@gmail.com>
2025-05-03 16:36:53 +0100

    init commit

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,2 +1,3 @@
 public class Administrador extends Atores{
+
 }
--- src/Atores.java
+++ src/Atores.java
@@ -3,4 +3,49 @@ public class Atores extends Federaçao {
     int numero;
     String curso;
     int password;
+
+    public Atores(String nome, int numero, String curso, int password){
+        this.nome=nome;
+        this.numero=numero;
+        this.curso=curso;
+        this.password=password;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + nome + curso + numero + password;
+    }
 }
+
